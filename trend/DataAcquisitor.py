8ca9a73c6f9fcf1f0cb56a184166fa4680c46e33
Qi Li <lq3552@gmail.com>
2023-06-24 00:30:02 +0200

    DataAcquisitor now tries to append data instead of acquire all data again

--- trend/DataAcquisitor.py
+++ trend/DataAcquisitor.py
@@ -2,6 +2,7 @@ from urllib.parse import urlencode
 import pandas as pd
 import requests
 import os
+import copy
 
 class DataAcquisitor(object):
 
@@ -30,9 +31,14 @@ class DataAcquisitor(object):
 	__fields = list(__EastmoneyKlines.keys())
 	__columns = list(__EastmoneyKlines.values())[1:]
 	__fields2 = ",".join(__fields)
+	__dateFormat = "%Y%m%d"
+	__emptyDataFrame = pd.DataFrame(columns = __columns, index = pd.DatetimeIndex(["1900-01-01"]))
 	_QuotationURLHeader = "https://xueqiu.com/S/"
 
-	def __init__(self, code: str, beg: str, end: str, isIndex: bool = False, mode: int = 0, inDir: str = ".", outDir: str = "."):
+	class UnsupportedDataFrameError(BaseException):
+		pass
+
+	def __init__(self, code: str, beg: str, end: str, isIndex: bool = False, mode: int = 0, inDir: str = None, outDir: str = "."):
 		'''
 		参数
 			code :  6 位股票代码
@@ -49,14 +55,15 @@ class DataAcquisitor(object):
 		self._beg   = beg
 		self._end   = end
 
-		self._inDir = inDir
 		self._outDir = outDir
+		if inDir == None:
+			inDir = outDir
+		self._inDir = inDir
+		self.read_from_csv()
 		if mode == 0:
 			self._dayK  = self._get_k_history(klt = 101)
 			self._weekK  = self._get_k_history(klt = 102)
 			self._monthK  = self._get_k_history(klt = 103)
-		else:
-			self.read_from_csv()
 
 	def get_code(self) -> str:
 		'''
@@ -88,11 +95,15 @@ class DataAcquisitor(object):
 
 	def read_from_csv(self):
 		try:
-			self._dayK =   pd.read_csv(f"{self._inDir}/{self._code}_day.csv", encoding="utf-8-sig", parse_dates = [0], index_col = 0)
-			self._weekK =  pd.read_csv(f"{self._inDir}/{self._code}_week.csv", encoding="utf-8-sig", parse_dates = [0], index_col = 0)
+			self._dayK   = pd.read_csv(f"{self._inDir}/{self._code}_day.csv", encoding="utf-8-sig", parse_dates = [0], index_col = 0)
+			self._weekK  = pd.read_csv(f"{self._inDir}/{self._code}_week.csv", encoding="utf-8-sig", parse_dates = [0], index_col = 0)
 			self._monthK = pd.read_csv(f"{self._inDir}/{self._code}_month.csv", encoding="utf-8-sig", parse_dates = [0], index_col = 0)
-		except:
-			print("File(s) not found!")
+			if self._dayK.empty or self._weekK.empty or self._monthK.empty:
+				raise self.UnsupportedDataFrameError()
+		except (FileNotFoundError, self.UnsupportedDataFrameError):
+			self._dayK   = copy.deepcopy(self.__emptyDataFrame)
+			self._weekK  = copy.deepcopy(self.__emptyDataFrame)
+			self._monthK = copy.deepcopy(self.__emptyDataFrame)
 
 	def save_to_csv(self):
 		if not os.path.exists(self._outDir):
@@ -144,10 +155,31 @@ class DataAcquisitor(object):
 	            前复权 : 1
 	            后复权 : 2 
 		'''
+
+		if klt == 101:
+			dfOld = self._dayK
+		elif klt == 102:
+			dfOld = self._weekK
+		elif klt == 103:
+			dfOld = self._monthK
+		else: # unsupported
+			return copy.deepcopy(self.__emptyDataFrame)
+
+		# find the date to append new rows
+		begOld = dfOld.index[0]
+		endOld = dfOld.index[-1]
+		beg = pd.DatetimeIndex([self._beg])[0]
+		end = pd.DatetimeIndex([self._end])[0]
+		if endOld > end:
+			return dfOld
+		beg = max(beg, endOld)
+		beg = beg.strftime(self.__dateFormat)
+		dfOld.drop(dfOld.index[-1], inplace = True) # drop the last row, because it could be updated during the market opening hours
+
 		params = (
 	        ("fields1", "f1,f2,f3,f4,f5,f6,f7,f8,f9,f10,f11,f12,f13"),
 	        ("fields2", self.__fields2),
-	        ("beg", self._beg),
+	        ("beg", beg),
 	        ("end", self._end),
 	        ("rtntype", '6'),
 	        ("secid", self._secid),
@@ -173,16 +205,19 @@ class DataAcquisitor(object):
 			data = json_response.get("data")
 		if data is None:
 			print("股票代码:", self._code, "可能有误")
-			return pd.DataFrame(columns = self.__columns)
+			return copy.deepcopy(self.__emptyDataFrame)
 	
 		klines = data['klines']
 
-		rows = []
 		index = []
+		rows  = dfOld.to_numpy().tolist()
 		for _kline in klines:
 			kline = _kline.split(',')
 			index.append(kline[0])
 			rows.append(kline[1:])
+
+		index = pd.DatetimeIndex(index)
+		index = pd.DatetimeIndex([*dfOld.index, *index])
 	
 		df = pd.DataFrame(rows, columns = self.__columns, index = index)
 
@@ -193,13 +228,13 @@ if __name__ == "__main__":
 	df = pd.read_csv('stock_codes/CSI300_component_codes.csv', dtype = {0: str})
 	header = df.columns[0]
 	# 股票代码
-	codes = df[header]
+	codes = df[header][:1]
 
 	# 开始日期
 	start_date = "20180621"
 	# 结束日期
-	end_date   = "20230621"
-	
+	end_date   = pd.to_datetime("today").strftime("%Y%m%d")
+	print(end_date)
 	size = len(codes)
 	i = 1
 	for code in codes:
