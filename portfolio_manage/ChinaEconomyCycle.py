362c8820e360c77309ac4440e25fccd9e9962007
Qi Li <lq3552@gmail.com>
2025-01-08 23:06:27 +0100

    Update ChinaEconomyCycle.py: align data by time; add plots that correlate PPI and CPI

--- portfolio_manage/ChinaEconomyCycle.py
+++ portfolio_manage/ChinaEconomyCycle.py
@@ -3,77 +3,98 @@ import statsmodels.api as sm
 import scipy.stats as stats
 
 class ChinaEconomyCycle(object):
-	def __init__(self, file_pmi = "economy_data/China_Composite_PMI.txt", file_ppi = "economy_data/China_PPI.txt", file_pmi_sec = "economy_data/China_Sector_PMI.txt"):
-		dat_pmi = np.loadtxt(file_pmi)
-		dat_ppi = np.loadtxt(file_ppi)
-		dat_pmi_sec = np.loadtxt(file_pmi_sec)
-		self.t = dat_pmi[:,0]
-		self.t = self.t // 100 + (self.t % 100 - 1) / 12
-		self.pmi_2nd  = dat_pmi_sec[:,1]
-		self.pmi_else = dat_pmi_sec[:,3]
-		self.pmi = dat_pmi[:,1]
-		self.pmi = (self.pmi - 50.0) * 2
-		self.ppi = dat_ppi[:self.pmi.shape[0],2]
+    def __init__(self, file_pmi = "economy_data/China_Composite_PMI.txt", file_ppi = "economy_data/China_PPI.txt", file_cpi = "economy_data/China_CPI.txt", file_pmi_sec = "economy_data/China_Sector_PMI.txt"):
+        '''
+        parameters: path(s) to economic data files
+            file_pmi, file_ppi, file_cpi, file_pmi_sec
+        '''
+        dat_pmi = np.loadtxt(file_pmi)
+        dat_ppi = np.loadtxt(file_ppi)
+        dat_cpi = np.loadtxt(file_cpi)
+        dat_pmi_sec = np.loadtxt(file_pmi_sec)
+        self.t = dat_pmi[: ,0]
+        self.pmi_2nd  = dat_pmi_sec[:,1]
+        self.pmi_else = dat_pmi_sec[:,3]
+        self.pmi = dat_pmi[:,1]
+        self.pmi = (self.pmi - 50.0) * 2
+        self.ppi = dat_ppi[:,2]
+        self.cpi = dat_cpi[:,2]
+        size = min(self.pmi.shape[0], self.ppi.shape[0], self.cpi.shape[0])
+        self.t = self.t[self.pmi.shape[0] - size:] // 100 + (self.t % 100 - 1) / 12
+        self.pmi = self.pmi[self.pmi.shape[0] - size:]
+        self.ppi = self.ppi[self.ppi.shape[0] - size:]
+        self.cpi = self.cpi[self.cpi.shape[0] - size:]
 
-	def HP_filter(self, lamb):
-		'''
-		lamb: lambda, The Hodrick-Prescott smoothing parameter. A value of 1600 is suggested for quarterly data. 
-		Ravn and Uhlig suggest using a value of 6.25 (1600/4**4) for annual data and 129600 (1600*3**4) for monthly data.
-		In practie, 100 and 14400 are commonly used respectively.
-		'''
-		x = self.t
-		y = self.pmi
-		pmi_cycle, pmi_trend = sm.tsa.filters.hpfilter(y, lamb = lamb)
-		y = self.ppi
-		ppi_cycle, ppi_trend = sm.tsa.filters.hpfilter(y, lamb = lamb)
-		return pmi_cycle, pmi_trend, ppi_cycle, ppi_trend
+    def HP_filter(self, lamb):
+        '''
+        lamb: lambda, The Hodrick-Prescott smoothing parameter. A value of 1600 is suggested for quarterly data. 
+        Ravn and Uhlig suggest using a value of 6.25 (1600/4**4) for annual data and 129600 (1600*3**4) for monthly data.
+        In practie, 100 and 14400 are commonly used respectively.
+        '''
+        y = self.pmi
+        pmi_cycle, pmi_trend = sm.tsa.filters.hpfilter(y, lamb = lamb)
+        y = self.ppi
+        ppi_cycle, ppi_trend = sm.tsa.filters.hpfilter(y, lamb = lamb)
+        y = self.cpi
+        cpi_cycle, cpi_trend = sm.tsa.filters.hpfilter(y, lamb = lamb)
+        return pmi_cycle, pmi_trend, ppi_cycle, ppi_trend, cpi_cycle, cpi_trend
 
-	def correlate_pmi_2nd_and_else(self):
-		print("(Person r, p-value) = ", stats.pearsonr(self.pmi_2nd, self.pmi_else))
+    def correlate_pmi_2nd_and_else(self):
+        print("(Person r, p-value) = ", stats.pearsonr(self.pmi_2nd, self.pmi_else))
 
 if __name__ == "__main__":
-	import matplotlib.pyplot as plt
-	from matplotlib.ticker import (MultipleLocator, AutoMinorLocator)
+    import matplotlib.pyplot as plt
+    from matplotlib.ticker import (MultipleLocator, AutoMinorLocator)
 
-	cec = ChinaEconomyCycle()
-	cec.correlate_pmi_2nd_and_else()
-	pmi_cycle, pmi_trend, ppi_cycle, ppi_trend = cec.HP_filter(lamb = 100)
-	dpmi_trend = pmi_trend[:-1] - pmi_trend[1:]
-	dppi_trend = ppi_trend[:-1] - ppi_trend[1:]
+    cec = ChinaEconomyCycle()
+    cec.correlate_pmi_2nd_and_else()
+    pmi_cycle, pmi_trend, ppi_cycle, ppi_trend, cpi_cycle, cpi_trend = cec.HP_filter(lamb = 100)
+    dpmi_trend = pmi_trend[:-1] - pmi_trend[1:]
+    dppi_trend = ppi_trend[:-1] - ppi_trend[1:]
+    dcpi_trend = cpi_trend[:-1] - cpi_trend[1:]
 
-	fig, ax = plt.subplots(nrows = 2, ncols = 2, sharex = True)
-	ax[0,0].plot(cec.t, pmi_trend, ls = "-", color = "C0", label = "Composite PMI - Pressure")
-	ax[0,0].plot(cec.t, ppi_trend, ls = "-", color = "C1", label = "PPI - Resistance")
-	ax[0,0].plot(cec.t, cec.pmi, ls = "--", marker = "", color = "C0")
-	ax[0,0].plot(cec.t, cec.ppi, ls = "--", marker = "", color = "C1")
-	ax[0,0].xaxis.set_minor_locator(MultipleLocator(0.25))
-	ax[0,0].xaxis.set_major_locator(MultipleLocator(1))
-	ax[0,0].set_ylabel("Index")
-	ax[0,0].legend()
+    fig, ax = plt.subplots(nrows = 3, ncols = 2, sharex = True)
 
-	ax[0,1].plot(cec.t[1:], dpmi_trend, ls = "-", color = "C0")
-	ax[0,1].plot(cec.t[1:], dppi_trend, ls = "-", color = "C1")
-	ax[0,1].xaxis.set_minor_locator(MultipleLocator(0.25))
-	ax[0,1].xaxis.set_major_locator(MultipleLocator(1))
-	ax[0,1].set_ylabel("Index Change")
+    for i in range(ax.shape[0]):
+        for j in range(ax.shape[1]):
+            ax[i, j].xaxis.set_minor_locator(MultipleLocator(0.25))
+            ax[i, j].xaxis.set_major_locator(MultipleLocator(1))
+    
+    ax[0,0].plot(cec.t, pmi_trend, ls = "-", color = "C0", label = "Composite PMI - Pressure")
+    ax[0,0].plot(cec.t, ppi_trend, ls = "-", color = "C1", label = "PPI - Resistance")
+    ax[0,0].plot(cec.t, cec.pmi, ls = "--", marker = "", color = "C0")
+    ax[0,0].plot(cec.t, cec.ppi, ls = "--", marker = "", color = "C1")
+    ax[0,0].set_ylabel("Index")
+    ax[0,0].legend()
 
-	ax[1,1].plot(cec.t[1:], np.sign(dpmi_trend), ls = "", marker = "o", color = "C0")
-	ax[1,1].plot(cec.t[1:], np.sign(dppi_trend), ls = "", marker = "o", color = "C1")
-	ax[1,1].xaxis.set_minor_locator(MultipleLocator(0.25))
-	ax[1,1].xaxis.set_major_locator(MultipleLocator(1))
-	ax[1,1].yaxis.set_major_locator(MultipleLocator(1))
-	ax[1,1].set_ylabel("Index Change Sign")
+    ax[0,1].plot(cec.t[1:], dpmi_trend, ls = "-", color = "C0")
+    ax[0,1].plot(cec.t[1:], dppi_trend, ls = "-", color = "C1")
+    ax[0,1].set_ylabel("Index Change")
+
+    ax[1,1].plot(cec.t[1:], np.sign(dpmi_trend), ls = "", marker = "o", color = "C0")
+    ax[1,1].plot(cec.t[1:], np.sign(dppi_trend), ls = "", marker = "o", color = "C1")
+    ax[1,1].yaxis.set_major_locator(MultipleLocator(1))
+    ax[1,1].set_ylabel("Index Change Sign")
+
+    ax[2,0].plot(cec.t, ppi_trend, ls = "-", color = "C0", label = "PPI")
+    ax[2,0].plot(cec.t, cpi_trend, ls = "-", color = "C1", label = "CPI")
+    ax[2,0].plot(cec.t, cec.ppi, ls = "--", marker = "", color = "C0")
+    ax[2,0].plot(cec.t, cec.cpi, ls = "--", marker = "", color = "C1")
+    ax[2,0].set_ylabel("Index")
+    ax[2,0].legend()
+
+    ax[2,1].plot(cec.t[1:], dppi_trend, ls = "-", color = "C0")
+    ax[2,1].plot(cec.t[1:], dcpi_trend, ls = "-", color = "C1")
+    ax[2,1].set_ylabel("Index Change")
 
 
-	stage = np.zeros_like(dpmi_trend)
-	for i in range(len(stage)):
-		if dpmi_trend[i] >= 0 and dppi_trend[i] < 0: stage[i] = 0 # recovery
-		elif dpmi_trend[i] >= 0 and dppi_trend[i] >= 0: stage[i] = 1 # expansion
-		elif dpmi_trend[i] < 0 and dppi_trend[i] >= 0: stage[i] = 2 # stagflation
-		else: stage[i] = 3 # recession
-	ax[1,0].plot(cec.t[1:], stage, ls = "-", marker = "o", color = "C2")
-	ax[1,0].xaxis.set_minor_locator(MultipleLocator(0.25))
-	ax[1,0].xaxis.set_major_locator(MultipleLocator(1))
-	ax[1,0].yaxis.set_major_locator(MultipleLocator(1))
-	ax[1,0].set_ylabel("Stage")
-	plt.show()
+    stage = np.zeros_like(dpmi_trend)
+    for i in range(len(stage)):
+        if dpmi_trend[i] >= 0 and dppi_trend[i] < 0: stage[i] = 0 # recovery
+        elif dpmi_trend[i] >= 0 and dppi_trend[i] >= 0: stage[i] = 1 # expansion
+        elif dpmi_trend[i] < 0 and dppi_trend[i] >= 0: stage[i] = 2 # stagflation
+        else: stage[i] = 3 # recession
+    ax[1,0].plot(cec.t[1:], stage, ls = "-", marker = "o", color = "C2")
+    ax[1,0].yaxis.set_major_locator(MultipleLocator(1))
+    ax[1,0].set_ylabel("Stage")
+    plt.show()
